c5dec1db3d160bf9be26db62fa2baed7d060ef85
Liv-5 <osmith5@kent.edu>
2025-02-17 15:41:40 -0500

    tiny fix

--- 02-Render-Deploy-Full-Stack/PERN-fullstack-example/server/src/controllers/controller.ts
+++ 02-Render-Deploy-Full-Stack/PERN-fullstack-example/server/src/controllers/controller.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const apiKey = process.env.TMDB_API_KEY;
+const apiKey = process.env.OMDB_API_KEY;
 
-const search = async (query) =>
-  axios.get(`https://api.themoviedb.org/3?apikey=trilogy&t=${query}&rating=pg`);
+const search = async (query: string) =>
+  axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${query}`);
 
 export default { search };
