37aeb43ff9a2fc636735b23d036ecf62b3ecd3c5
Devilwife <nicole.barger22@gmail.com>
2025-02-18 13:30:49 -0500

    updating

--- 02-Render-Deploy-Full-Stack/PERN-fullstack-example/client/src/components/SearchForm.tsx
+++ 02-Render-Deploy-Full-Stack/PERN-fullstack-example/client/src/components/SearchForm.tsx
@@ -23,23 +23,6 @@ function SearchForm({ search, setSearch, onSearchSubmit }: SearchFormProps) {
     setSearch("");
   };
 
-import React from "react";
-
-interface SearchFormProps {
-  value: string;
-  name: string;
-  type: string;
-  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleFormSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-}
-
-const SearchForm: React.FC<SearchFormProps> = ({
-  value,
-  name,
-  type,
-  handleInputChange,
-  handleFormSubmit,
-}) => {
   return (
     <form onSubmit={handleFormSubmit}>
       <div className="form-group">
@@ -61,25 +44,5 @@ const SearchForm: React.FC<SearchFormProps> = ({
     </form>
   );
 }
-
-const SearchForm: React.FC<SearchFormProps> = ({
-  value,
-  name,
-  type,
-  handleInputChange,
-  handleFormSubmit,
-}) => {
-  return (
-    <form onSubmit={handleFormSubmit}>
-      <input
-        value={value}
-        name={name}
-        type={type}
-        onChange={handleInputChange}
-      />
-      <button type="submit">Search</button>
-    </form>
-  );
-};
 
 export default SearchForm;
