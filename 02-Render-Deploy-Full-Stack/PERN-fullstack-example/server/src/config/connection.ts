9693fa6acaa34e248b6f1a847d094d714d18d4a2
Liv-5 <osmith5@kent.edu>
2025-02-18 12:41:24 -0500

    trying to deploy

--- 02-Render-Deploy-Full-Stack/PERN-fullstack-example/server/src/config/connection.ts
+++ 02-Render-Deploy-Full-Stack/PERN-fullstack-example/server/src/config/connection.ts
@@ -1,34 +1,49 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
 dotenv.config(); //todo Load environment variables
 
+console.log("Connecting to database:", process.env.DB_NAME || "NOT SET");
+console.log("Using user:", process.env.DB_USER || "NOT SET");
 
-console.log('Connecting to database:', process.env.DB_NAME || 'NOT SET');
-console.log('Using user:', process.env.DB_USER || 'NOT SET');
+let sequelize;
 
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || "reelgenius_db",
+    process.env.DB_USER || "postgres",
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "postgres",
+    }
+  );
+}
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'reelgenius_db',  
-  process.env.DB_USER || 'postgres', 
-  process.env.DB_PASSWORD,  
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-    logging: false,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || 'reelgenius_db',
+//   process.env.DB_USER || 'postgres',
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       decimalNumbers: true,
+//     },
+//     logging: false,
+//   }
+// );
 
 //todo Check database connection
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Connection to reelgenius_db established successfully.');
+    console.log("Connection to reelgenius_db established successfully.");
   })
   .catch((error) => {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   });
 
 export default sequelize;
