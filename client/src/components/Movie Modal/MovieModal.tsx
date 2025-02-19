144c80f987b1b9384fb892718112f786472af1e2
Liv-5 <osmith5@kent.edu>
2025-02-19 10:59:31 -0500

    working on modal

--- client/src/components/Movie Modal/MovieModal.tsx
+++ client/src/components/Movie Modal/MovieModal.tsx
@@ -1,21 +1,32 @@
 //Todo: Import RecsContainer from RecsContainer
-
 //Todo: Import styles as needed
-
 //Todo: Import OMDBcontainer to fetch movie data
+import OmdbContainer from "../OMDBcontainer";
 
 //Todo: Create a container to display OMDBContainer data
+function Modal() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="App">
+      <Button variant="primary" onClick={toggle}>
+        Send
+      </Button>
+      <MyModal isOpen={isOpen} toggle={toggle}>
+        <p>This is the content.</p>
+      </MyModal>
+    </div>
+  );
+}
 
 //Todo: Stylize the container to match the wireframe
 
 //Todo: Add Recscontainer to display movie recommendations
 
-
 //Todo: Add like buttons to add to User's favorite movies
-
 
 //Todo: HTML for exporting the MovieModal component
 
-
-
 //Todo: export MovieModal
+export default MovieModal;
