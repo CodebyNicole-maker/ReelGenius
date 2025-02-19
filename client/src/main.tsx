6703e78573233b51c15a164b32eed2f1cb5d6ac2
Pedro E. <104291673+aMononito@users.noreply.github.com>
2025-02-19 17:45:02 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -31,6 +31,10 @@ const router = createBrowserRouter([
         path: "/signup",
         element: <SignUp />,
       },
+      {
+        path: '/reviews',
+        element: <Reviews />
+      }, 
       // {
       //   path: "/profile",
       //   element: <Profile />,
